058f567f3df94742742ec3ed18b2257e5937e42a
Vijay M <vijaymurugan177@gmail.com>
2023-04-21 21:03:29 +0530

    ADDED: Welcome

---
+++ components/TokenSection.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Hero() {
+    return (
+     <section className="py-24 px-10 text-white">
+        
+     </section>
+    );
+  }
+  
--- components/WelsomSection.jsx
+++ components/WelsomSection.jsx
@@ -38,20 +38,34 @@ export default function Welcome() {
       <div className="flex flex-col-reverse md:flex-row-reverse justify-center md:space-x-10 space-y-10 ">
         <div className="chibiki_left w-ful md:w-1/2  text-center space-y-8">
           <div className="chibiki_text text-center space-y-8">
+            <h2 className="text-4xl uppercase font-black -mb-3 -rotate-3 bg-shine">
+              gaming
+            </h2>
+
             <h1
               className="font-bold text-5xl leading-tight	
  "
             >
               Taking Gaming to the Next Level
             </h1>
-            <p>
-              The Most Cutest and Strongest Anime style NFT collection on SUI
-              with multiple utilities backed by the NFT and token.
-            </p>
+            <ul className="space-y-5">
+              <li>
+                <h2 className="font-semibold uppercase italic mb-2 bg-shine">Chibiki Run</h2>
+                <p>
+                An Infinite Runner Mini game with more enemies and obstacles. Let’s see who scores the highest
+                </p>
+              </li>
+              <li>
+                <h2 className="font-semibold uppercase italic mb-2 bg-shine">Chibikis - Origin</h2>
+                <p>
+                A Platformer game (Metroidvania style) set in a dystopian world thats different from Earth’s reality.
+                </p>
+              </li>
+            </ul>
           </div>
           <div className="chibiki_btn flex justify-around">
             <a
-              className="py-3 px-6 w-40 rounded-full bg-green-500 hover:scale-110 transition-all active:scale-90"
+              className="py-3 px-6 w-40 rounded-full bg-green-500 hover:scale-110 transition-all active:scale-90 hover:-rotate-3"
               href=""
             >
               Play Now
