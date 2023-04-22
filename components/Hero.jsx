07565ce362cb22d11aeff931b4df59d277debd3a
Vijay M <vijaymurugan177@gmail.com>
2023-04-22 16:39:18 +0530

    CONVERTED: to svg

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="relative w-full">
         <div className="flex justify-between h-screen w-screen absolut items-center">
           <img
-            src="/img/hero-left.png"
+            src="/img/hero-left.svg"
             alt="chibiki"
             className="hidden lg:block w-72 h-fit ml-24 p-4 floating"
           />
@@ -19,13 +19,13 @@ export default function Hero() {
               />
             </div>
             <img
-              src="/img/hero-img.png"
+              src="/img/hero-img.svg"
               alt="chibikies"
               className="w-[45rem] -mb-8  py-3"
             />
           </div>
           <img
-            src="/img/hero-right.png"
+            src="/img/hero-right.svg"
             alt="chibiki"
             className="hidden lg:block md:w-96 h-fit mr-20 p-4 floating"
           />
